ebdb529faacb5d135ec16f979004439728aa6863
김형섭 <hs8316@gmail.com>
2025-01-30 15:54:55 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+
+		String [] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="} ;
+
+		for(int i = 0 ; i < arr.length ; i++) {
+
+			str = str.replace(arr[i], "?");
+		}
+		System.out.println(str.length());
+	}
+}
